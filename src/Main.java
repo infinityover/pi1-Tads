7a6e2fed5148a0b0a28ef26136d6d13ef3ccd4e4
ValdiranCruz <93160148+ValdiranCruz@users.noreply.github.com>
2021-11-19 22:49:02 -0300

    Adicionando a questão 2

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,35 @@ import java.lang.reflect.Array;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 
+
+
 public class Main {
     //Perguntas
     private static String p1 = "Por que tudo junto se escreve separado, e separado se escreve tudo junto?";
-
+private static String p2 =("elseif (nota == 10) {\r\n"
+		+ "			System.out.println(nome + \" Parabéns você ganhou uma     bolsa de estudos\");\r\n"
+		+ "		} else if (nota > 7 && nota < 10) {\r\n"
+		+ "			System.out.println(nome + \"você ganhou 20% de desconto na mensalidade\");\r\n"
+		+ "		} else if (nota >= 05 && nota < 7) {\r\n"
+		+ "			System.out.println(nome + \" você ganhou desconto de 10% na mensalidade\");\r\n"
+		+ "		} else if nota <= 5 || nota == 0 {\r\n"
+		+ "			System.out.println(nome + \" Infelizmente você não Passou no processo seletivo\");\r\n"
+		+ "		}\n O código acima mostra a verificação de um processo seletivo de um aluno\r\n"
+		+ "	 Identifique os erros que está impedindo o programa executar corretamente.\n"
+		+ "");
 
     //Respostas
     private static ArrayList<String> r1 = new ArrayList<>(Arrays.asList("por que sim(CERTA RESPOSTA)","por que foi feito assim","porque sim","porque nÃ£o?","e eu sei lÃ¡?"));
-
+    private static ArrayList<String> r2 = new ArrayList<>(Arrays.asList("A estrutura IF não serve para verificar condicional. ", "A estrutura é iniciada primeiro com IF e a condição é entre (). (CERTA RESPOSTA)",
+    		"Tem que declarar uma variável dentro da condição para poder executar o código.", "A estrutura não pode ter um IF dentro de outro IF."));
+    
     //Historias
     private static String h1 = "Uhmmm, hoje Ã© um excelente dia para filosofar...";
 
 
     public static void main(String[] args) throws InterruptedException {
         historia(1);
-        pergunta(1);
+        pergunta(2);
     }
 
     //id = id da pergunta
@@ -35,6 +49,8 @@ public class Main {
                 imprimir(p1, 10l);
 
                 System.out.println(imprimirQuestao(r1));
+            case 2: imprimir(p2, 10l);
+            System.out.println(imprimirQuestao(r2));
         }
     }
 
