8868d9df5e289e43fb91b341314f2ee3d3c3f317
ValdiranCruz <93160148+ValdiranCruz@users.noreply.github.com>
2021-11-21 00:32:10 -0300

    Adicionando Alternativas

--- src/Main.java
+++ src/Main.java
@@ -7,44 +7,46 @@ public class Main {
 	private static String p1 = "Por que tudo junto se escreve separado, e separado se escreve tudo junto?";
 
 	private static String p2 = "Escolha a alternativa correta que descreve o conceito de IF e IF ELSE? "
-			+ "    A estrutura IF executa um comando sempre que a condição seja falsa\r\n"
-			+ "    A estrutura IF executa um comando quando a condicional seja verdadeira correta\r\n"
-			+ "    Podemos afirmar que o IF ELSE executa o comando e depois faz a verificação\r\n"
-			+ "    A estrutura IF ELSE executa um conjunto de comandos caso a condição IF seja falsa\r\n"
+			+ "    A estrutura IF executa um comando sempre que a condição seja falsa\n"
+			+ "    A estrutura IF executa um comando quando a condicional seja verdadeira correta\n"
+			+ "    Podemos afirmar que o IF ELSE executa o comando e depois faz a verificação\n"
+			+ "    A estrutura IF ELSE executa um conjunto de comandos caso a condição IF seja falsa\n"
 			+ "    Podemos afirmar que IF ELSE é utilizado para o teste de múltiplos caso seria IF ELSE aninhado correta";
 
-	private static String p3 = ("elseif (nota == 10) {\r\n"
-			+ "			System.out.println(nome + \" Parabéns você ganhou uma     bolsa de estudos\");\r\n"
-			+ "		} else if (nota > 7 && nota < 10) {\r\n"
-			+ "			System.out.println(nome + \"você ganhou 20% de desconto na mensalidade\");\r\n"
-			+ "		} else if (nota >= 05 && nota < 7) {\r\n"
-			+ "			System.out.println(nome + \" você ganhou desconto de 10% na mensalidade\");\r\n"
-			+ "		} else if nota <= 5 || nota == 0 {\r\n"
-			+ "			System.out.println(nome + \" Infelizmente você não Passou no processo seletivo\");\r\n"
-			+ "		}\n O código acima mostra a verificação de um processo seletivo de um aluno\r\n"
+
+	private static String p3 = ("elseif (nota == 10) {\n"
+			+ "			System.out.println(nome + \" Parabéns você ganhou uma     bolsa de estudos\");\n"
+			+ "		} else if (nota > 7 && nota < 10) {\n"
+			+ "			System.out.println(nome + \"você ganhou 20% de desconto na mensalidade\");\n"
+			+ "		} else if (nota >= 05 && nota < 7) {\n"
+			+ "			System.out.println(nome + \" você ganhou desconto de 10% na mensalidade\");\n"
+			+ "		} else if nota <= 5 || nota == 0 {\n"
+			+ "			System.out.println(nome + \" Infelizmente você não Passou no processo seletivo\");\n"
+			+ "		}\n O código acima mostra a verificação de um processo seletivo de um aluno\n"
 			+ "	 Identifique os erros que está impedindo o programa executar corretamente.\n" + "");
 
 	private static String p4 = "Escolha a opção que descreve como o break funciona quando é utilizado dentro de um laço de repetição aninhado.";
 
 	private static String p5 = "O laço de repetição abaixo está formando uma metade de uma pirâmide, sendo criada por linhas e colunas. Porém o código não está rodando, o porquê o laço de repetição for não está rodando?"
-			+ "\"for (int linha = 0; linha < 10; linha + linha) {\"\r\n"
-			+ "	 for (int coluna = 0; coluna <= 10; coluna++) {\"\r\n" + "		if (coluna > linha)\"\r\n"
-			+ "			break;\"\r\n" + "				System.out.print(\\\" *\\\");\"\r\n" + "			}\"\r\n"
-			+ "			System.out.println();\"\r\n" + "		}\"\r\n";
+			+ "\"for (int linha = 0; linha < 10; linha + linha) {\"\n"
+			+ "	 for (int coluna = 0; coluna <= 10; coluna++) {\"\n" + "		if (coluna > linha)\"\n"
+			+ "			break;\"\n" + "				System.out.print(\" *\");\"\n" + "			}\"\n"
+			+ "			System.out.println();\"\n" + "		}\"\n";
 
 	// Respostas
 	private static ArrayList<String> r1 = new ArrayList<>(Arrays.asList("por que sim(CERTA RESPOSTA)",
 			"por que foi feito assim", "porque sim", "porque nÃ£o?", "e eu sei lÃ¡?"));
 
-	private static ArrayList<String> r2 = new ArrayList<>(Arrays.asList("(I e II) Estão corretas",
+	private static ArrayList<String> r2 = new ArrayList<>(Arrays.asList("(I e II e VI) Estão corretas",
 			"(I e II e III) Estão corretas", "(II e V) Estão corretas (CERTA RESPOSTA)", "(IV e II) Estão corretas",
-			"(I e IV e V) Estão corretas)"));
+			"(I e IV e V) Estão corretas)", "I ) Estão corretas"));
 
 	private static ArrayList<String> r3 = new ArrayList<>(
 			Arrays.asList("A estrutura IF não serve para verificar condicional. ",
 					"A estrutura é iniciada primeiro com IF e a condição é entre (). (CERTA RESPOSTA)",
 					"Tem que declarar uma variável dentro da condição para poder executar o código.",
-					"A estrutura não pode ter um IF dentro de outro IF."));
+					"A estrutura não pode ter um IF dentro de outro IF.",
+					"A estrutura sempre terá um IF dentro de outro IF"));
 
 	private static ArrayList<String> r4 = new ArrayList<>(Arrays.asList(
 			"Finaliza a execução do programa no momento em que é chamado.",
@@ -124,7 +126,7 @@ public class Main {
 				respostaCerta = Character.toString((char) i + 97);
 			}
 			System.out.println(
-					Character.toString((char) i + 97) + ") " + perguntas.get(i).replaceAll("\\(CERTA RESPOSTA\\)", ""));
+					Character.toString((char) i + 97) + ") " + perguntas.get(i).replaceAll("(CERTA RESPOSTA)", ""));
 		}
 		String resp = scan.next();
 		return respostaCerta.toUpperCase(Locale.ROOT).equals(resp.toUpperCase(Locale.ROOT));
